46b309a488ec30d2ee55fa4e06f9d4db5779254d
Leandro <leoviolinist22@gmail.com>
2022-09-21 17:01:52 -0300

    colocando subsequencias

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,8 @@
 #include <ctype.h> //Adiciona o isalnum
 
 
-#define TAMANHO 100;
 
-int t[100][100];  
+int **t;  
 
 
 
@@ -23,7 +22,6 @@ typedef struct Subsequence {
 
 	inline static Subsequence Concatenate(Subsequence& sigma_1, Subsequence& sigma_2) {
 		Subsequence sigma; // Subsequencia gerada pela concatenação
-		
 		/* Tempo do último nó da subsequência 1, até o primeiro nó da segunda	*/
 		double temp = t[sigma_1.last][sigma_2.first];
 		sigma.W = sigma_1.W + sigma_2.W; // Calculo do custo de atraso
@@ -110,7 +108,7 @@ Solution * Construction(int dimensao, int** matriz) {
 			CL.erase(it);
 		}
 		
-		r = RCL[selecionado];
+		r = RCL[selecionado]; // Atualiza o ultimo nó adicionado
 		insercaoIndice++;
 
 
@@ -124,8 +122,7 @@ Solution * Construction(int dimensao, int** matriz) {
 
 void updateAllSubseq(Solution *s, std::vector<std::vector< Subsequence >> &subseq_matrix) {
 	
-	int n =  s->sequence.size(); // Numero de nós da instancia
-
+	int n =  s->sequence.size() - 1; // Numero de nós da instancia, retira 1, pois a solução é 1 2 3 4 5 1, porém ela só tem 5 nós não 6.
 	/*Atualiza as subsequencias de um unico nó
 	 * O custo de atraso = 0 se for o primeiro nó, caso não é 1
 	 * O custo = 0
@@ -137,15 +134,47 @@ void updateAllSubseq(Solution *s, std::vector<std::vector< Subsequence >> &subse
 	 * Lembrando que, subseq_matrix[i][j] contém as informações da subsequencia que vai de i ate j*/
 	for(int i = 0; i < n; i++) {
 		
-		int v = s->sequence[i];
-
+		int v = s->sequence[i] - 1; // Vamos retirar 1, porque para acessar a matriz que vai de 0 até 13, a sequencia vai de 1 ate 14, dado n = 14
 		subseq_matrix[i][i].W = (i > 0);
 		subseq_matrix[i][i].C = 0;
 		subseq_matrix[i][i].T = 0;
-		subseq_matrix[i][i].first = v;
+		subseq_matrix[i][i].first = v; 
 		subseq_matrix[i][i].last = v;
+	
+		
 	}
+	
+	/*
+	 * Se temos a solução s = 1, 2, 5, 7, 8, 9, 1
+	 * i = 0 e j = 1 ( subsequencia que vai de 0 ate 1 )
+	 * Temos a subsequencia que vai de 1 ate 2 em s, ela é formada pela concatenação da subsequencia de 0 ate 0  + 1 até 1
+	 * i = 0 e j = 2 ( subsequencia que vai de 0 ate 2 )
+	 * Temos a subsequencia que vai de 1 ate 5 em s, ela é formada pela concatenação da subsequencia de 0 até 1 + subsequencia de 2 até 2
+	 * i = 0 e j =3 ( subsequencia que vai de 0 ate 3 )
+	 * Temos a subsequencia que vai de 1 até 7 em s, ela é formada pela concatenação da subsequencia de 0 até 2 + subsequencia de 3 até 3	*/
+	for(int i = 0; i < n; i++) {
 
+		for(int j = i + 1; j < n; j++) {
+			subseq_matrix[i][j] = Subsequence::Concatenate(subseq_matrix[i][j - 1], subseq_matrix[j][j]);
+		}
+	}
+	/* Subsequencias invertidas
+	 * Dado n = 5; logo o maior índice é 4
+	 * i = 4 e j = 3 (subsequencia que vai de 4 até 3)
+	 * Ela é formada pela concatenação da subsequencia de 4 até 4 + a subsequencia de 3 até 3	
+	 * i = 4 e j = 2 (subsequencia que vai de 4 até 2)
+	 * Ela é formada pela concatenação da subsequencia de 4 até 3 + a subsequencia de 2 ate 2*/
+	/*	Perceba que as concatenações sempre são feitas a partir de uma matriz criada anteriormente  + elemento da diagonal principal	*/
+	for(int i = n - 1; i >= 0; i--) {
+		
+		for(int j = i - 1; j >= 0; j--) {
+
+			subseq_matrix[i][j] = Subsequence::Concatenate(subseq_matrix[i][j + 1], subseq_matrix[j][j]);
+		}
+
+	}
+	
+	std::cout << "Atualizou as subsequencias\n";
 }
 
 int main() {
@@ -183,26 +212,28 @@ int main() {
 			std::cin >> matriz[i][j];
 		}
 	}
-	
+	t = matriz; // atualiza o vetor de custos que será utilizado na concatenação
+ 
 	/*-------------------------------------------------*/
 
 	Solution* s = Construction(dimensao, matriz);
 	
-	/*
+	
 	std::cout << "sequencia apos construcao" << std::endl;
 	for(int i = 0; i < s->sequence.size(); i++) {
 
 		std::cout << s->sequence[i] << " ";
 	}
-	std::cout << "-------------------------------------------" << std::endl;
-	*/
+	std::cout << std::endl;
 
 	/* Matriz com todas as subsequencia
-	 * a subseq_matrix[i][j] armazena informações da subsequencia que inicia em i e termina em j	*/
-
+	 * a subseq_matrix[i][j] armazena informações da subsequencia que inicia em i e termina em j relativo a solução s	*/
+	
+	
 	std::vector < std::vector < Subsequence > > subseq_matrix(dimensao, std::vector < Subsequence >(dimensao));
 	
-
+	
+	updateAllSubseq(s, subseq_matrix);
 	for(int i = 0; i < dimensao; i++) {
 
 
